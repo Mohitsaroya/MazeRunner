626ad18f787c9d5f88d09208bb1a667daa20ad40
Mohit Saroya <mohitsaroya999@gmail.com>
2025-11-23 19:26:41 -0700

    Revert "initial commit, support files and initial folder structure set up."
    This reverts commit 435b7f13189af3dbe8c49fc7b8cf4484d590cd38.

--- SupportFiles/ncurses/W10_L3_CursesCodes/Ex8/Fun.h
+++
@@ -1,7 +0,0 @@
-#include<ncurses.h>
-void printSnake(WINDOW *gamewin,int length
-	, char *body, int *yLoc, int *xloc);
-
-void mvSnake(WINDOW *gamewin,int length
-	, char *body, int *yLoc, int *xloc,char* dir, int pressK);
-
--- SupportFiles/testing/Ex2/testUnit.h
+++
@@ -1,5 +0,0 @@
-#ifndef __testUnit_H
-#define __testUnit_H
-void test_maxi(void);
-#endif
-
