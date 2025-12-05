1e084b2ba185fdfde43f100857583bdc2fc0cb48
sademojifacesad <wonge64@mymacewan.ca>
2025-12-04 19:16:18 -0700

    Fix formatting and comments in level1.h

--- A2/Evan/level1.h
+++ A2/Evan/level1.h
@@ -9,16 +9,17 @@
 
 /**
 * @Brief first_level_maze makes the level 1 maze and general gameplay
-* @param void
-* @return it return global  macros based on if the player wants to quit,menu,next or  pause
+* @Param void
+* @Return it return global  macros based on if the player wants to quit,menu,next or  pause
 */
 int first_level_maze(void);
 
 /**
 * @Brief level1Phase calls for the level 1 splash screen and calls for first_level_maze
-* @param void
-* @return it return global macros based on if the player wants to quit,menu, or next
+* @Param void
+* @Return it return global macros based on if the player wants to quit,menu, or next
 */
 int level1Phase(void);
 
 #endif
+
